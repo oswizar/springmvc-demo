2a94fb253a0b14698a4fd6dad4c738409b89a0cf
xiexing <xiexing_qd@caxins.com>
2019-01-10 19:37:38 +0800

    first commit

---
+++ src/main/java/com/xiexing/controller/HelloWorldController.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C): 长安新生(深圳)金融投资有限公司
+ * FileName: HelloWorldController
+ * Author:   xiexing
+ * Date:     2019/1/10 16:55
+ * Description:
+ */
+package com.xiexing.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+//@RequestMapping("/spring")
+public class HelloWorldController {
+
+    @RequestMapping("/helloworld")
+    public String helloWorld() {
+        System.out.println("hello world!");
+        return "success";
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8"%>
+<!DOCTYPE html >
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>index</title>
+</head>
+<body>
+<%--1.可以访问--%>
+<%--<a href="<%=request.getContextPath()%>/helloworld">Hello World</a>--%>
+
+<%--2.也可以访问--%>
+<a href="helloworld">Hello World</a>
+
+</body>
+</html>
